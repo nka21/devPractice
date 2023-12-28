4cd43e862e529d28c9e7dbb37e735c340f102bcc
nka21 <133028205+nka21@users.noreply.github.com>
2023-12-28 23:39:50 +0900

    [追加] 27日の進捗
    プログラミングチュートリアルさんのReact入門見ながらやってた

---
+++ react/react-app-tutorial/src/App.js
@@ -0,0 +1,30 @@
+import { useState, useRef } from 'react';
+import TodoList from './TodoList';
+
+function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, name: "Todo1", completed: false }
+  ]);
+
+  const todoNameRef = useRef();
+
+  const handleAddTodo = () => {
+    //タスクを追加する処理
+    const name = todoNameRef.current.value;
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: "1", name: name, completed: false }];
+    });
+  };
+
+  return (
+    <>
+      <TodoList todos={todos} />
+      <input type="text" ref={todoNameRef} />
+      <button onClick={handleAddTodo}>タスクを追加</button>
+      <button>完了したタスクの削除</button>
+      <div>残りのタスク:0</div>
+    </>
+  );
+}
+
+export default App;
