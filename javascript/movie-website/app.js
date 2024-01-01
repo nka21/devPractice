4a93463dc927dde26c34abd4863b924311cf7425
nka21 <133028205+nka21@users.noreply.github.com>
2024-01-02 00:01:50 +0900

    終わりました〜

--- javascript/movie-website/app.js
+++ javascript/movie-website/app.js
@@ -5,8 +5,9 @@ arrows.forEach((arrow, i) => {
   const itemNumber = movieLists[i].querySelectorAll("img").length;
   let clickCounter = 0;
   arrow.addEventListener("click", () => {
+    const ratio = Math.floor(window.innerWidth / 300);
     clickCounter++;
-    if (itemNumber - (6 + clickCounter) >= 0) {
+    if (itemNumber - (6 + clickCounter) + (6 - ratio) >= 0) {
       movieLists[i].style.transform = `translateX(${
         movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
       }px)`;
@@ -15,6 +16,17 @@ arrows.forEach((arrow, i) => {
       clickCounter = 0;
     }
   });
+});
 
-  console.log();
+//TOGGLE
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(
+  ".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle"
+);
+
+ball.addEventListener("click", () => {
+  items.forEach((item) => {
+    item.classList.toggle("active");
+  });
+  ball.classList.toggle("active");
 });
